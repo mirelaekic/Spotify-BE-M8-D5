a25f4792c706791b75d8e4fae4f357286708b931
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2021-02-19 11:00:55 +0100

    Update tool.js

--- src/services/auth/tool.js
+++ src/services/auth/tool.js
@@ -1,90 +1,88 @@
 const jwt = require("jsonwebtoken");
-const user = require("../users/schema");
+const UserModel = require("../users/schema");
 
-const authenticate = async (user) => {
-  try {
-    const newToken = await generateJWT({ _id: user._id });
-    const newRefreshToken = await generateRefreshJWT({ _id: user._id });
+const authenticate = async user => {
+    try {
+      // create tokens
+      const accessToken = await generateAccessToken({ _id: user._id })
+      const refreshToken = await generateRefreshToken({ _id: user._id })
 
-    user.refreshTokens = user.refreshTokens.concat({
-      token: newRefreshToken,
-    });
-    await user.save();
-    return { token: newToken, refreshToken: newRefreshToken };
-  } catch (error) {
-    throw new Error(error);
-    console.log(error);
+      user.refreshTokens = user.refreshTokens.concat({ token: refreshToken })
+      await user.save()
+
+      return { accessToken, refreshToken }
+    } catch (error) {
+      console.log(error)
+      throw new Error(error)
+    }
   }
-};
-
-const generateJWT = (playload) =>
-  new Promise((res, rej) =>
-    jwt.sign(
-      playload,
-      process.env.JWT_SECRET,
-      { expiresIn: 5 },
-      (err, token) => {
-        if (err) rej(err);
-        res(token);
-      }
+  
+  const generateAccessToken = payload =>
+    new Promise((res, rej) =>
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        { expiresIn: "15m" },
+        (err, token) => {
+          if (err) rej(err)
+          res(token)
+        }
+      )
     )
-  );
-
-const verifyJWT = (token) =>
-  new Promise((res, rej) =>
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) rej(err);
-      res(decoded);
-    })
-  );
-
-const generateRefreshJWT = (payload) =>
-  new Promise((res, rej) =>
-    jwt.sign(
-      payload,
-      process.env.REFRESH_JWT_SECRET,
-      { expiresIn: "1 week" },
-      (err, token) => {
-        if (err) rej(err);
-        res(token);
-      }
+  
+  const verifyAccessToken = token =>
+    new Promise((res, rej) =>
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) rej(err)
+        res(decoded)
+      })
     )
-  );
-
-const verifyRefreshToken = (token) =>
-  new Promise((res, rej) =>
-    jwt.verify(token, process.env.REFRESH_JWT_SECRET, (err, decoded) => {
-      if (err) rej(err);
-      res(decoded);
-    })
-  );
-const refreshToken = async (oldRefreshToken) => {
-  const decoded = await verifyRefreshToken(oldRefreshToken);
-
-  const findUser = await user.findOne({ _id: decoded._id });
-
-  if (!findUser) {
-    throw new Error(`Access is forbidden`);
+  
+  const generateRefreshToken = payload =>
+    new Promise((res, rej) =>
+      jwt.sign(
+        payload,
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: "1 week" },
+        (err, token) => {
+          if (err) rej(err)
+          res(token)
+        }
+      )
+    )
+  
+  const verifyRefreshToken = token =>
+    new Promise((res, rej) =>
+      jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+        if (err) rej(err)
+        res(decoded)
+      })
+    )
+  
+  const refresh = async oldRefreshToken => {
+    try {
+      const decoded = await verifyRefreshToken(oldRefreshToken) 
+      const user = await UserModel.findOne({ _id: decoded._id })
+  
+      const currentRefreshToken = user.refreshTokens.find(
+        token => token.token === oldRefreshToken
+      )
+      if (!currentRefreshToken) {
+        throw new Error("Bad refresh token provided!")
+      }  
+      const newAccessToken = await generateAccessToken({ _id: user._id })
+      const newRefreshToken = await generateRefreshToken({ _id: user._id })
+      const newRefreshTokensList = user.refreshTokens
+        .filter(token => token.token !== oldRefreshToken)
+        .concat({ token: newRefreshToken })
+  
+      user.refreshTokens = [...newRefreshTokensList]
+      await user.save()
+  
+      return { accessToken: newAccessToken, refreshToken: newRefreshToken }
+    } catch (error) {
+      console.log(error)
+    }
   }
-  const currentRefreshToken = user.refreshTokens.find(
-    (t) => t.token === oldRefreshToken
-  );
-
-  if (!currentRefreshToken) {
-    throw new Error(`Refresh token is wrong`);
-  }
-
-  const newAccessToken = await generateJWT({ _id: user._id });
-  const newRefreshToken = await generateRefreshJWT({ _id: user._id });
-
-  const newRefreshTokens = user.refreshTokens
-    .filter((t) => t.token !== oldRefreshToken)
-    .concat({ token: newRefreshToken });
-
-  user.refreshTokens = [...newRefreshTokens];
-
-  await user.save();
-
-  return { token: newAccessToken, refreshToken: newRefreshToken };
-};
-module.exports = { authenticate, verifyJWT, refreshToken };
+  
+  module.exports = { authenticate, verifyAccessToken, refresh }
